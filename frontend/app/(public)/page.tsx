668b18cee25bee571287445c6c00a6efa4a8ecb1
Danie1l6 <danielsierra103@gmail.com>
2026-04-17 23:43:09 -0500

    fix(home): centrar proyectos destacados con flex justify-center

--- frontend/app/(public)/page.tsx
+++ frontend/app/(public)/page.tsx
@@ -113,9 +113,11 @@ export default async function HomePage() {
               </Link>
             </div>
 
-            <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+            <div className="flex flex-wrap justify-center gap-6">
               {featuredProjects.map((project) => (
-                <ProjectCard key={project.id} project={project} />
+                <div key={project.id} className="w-full sm:w-[calc(50%-12px)] lg:w-[calc(33.333%-16px)]">
+                  <ProjectCard project={project} />
+                </div>
               ))}
             </div>
 
