03c9bfd65ff8680ca60edd43600da25919e7c487
Danie1l6 <danielsierra103@gmail.com>
2026-07-30 00:01:13 -0500

    fix: scroll de modales

--- resources/views/components/admin/form-modal.blade.php
+++ resources/views/components/admin/form-modal.blade.php
@@ -14,16 +14,24 @@
         'xl' => 'max-w-3xl',
         default => 'max-w-lg',
     };
-    $modalClasses = trim("admin-form-modal {$sizeClass} ".$attributes->get('class', ''));
+    $isScrollable = $size === 'xl';
+    $modalClasses = trim("admin-form-modal admin-form-modal--size-{$size} ".($isScrollable ? 'admin-form-modal--scrollable ' : '')."{$sizeClass} ".$attributes->get('class', ''));
+    $isDestructive = str_contains(strtolower($name), 'delete');
+    $headerIcon = $isDestructive ? 'trash' : (str_contains(strtolower($title), 'editar') ? 'pencil-square' : 'plus');
 @endphp
 
-<flux:modal :name="$name" wire:model="{{ $model }}" :class="$modalClasses" :closable="false">
+<flux:modal :name="$name" wire:model="{{ $model }}" :class="$modalClasses.($isDestructive ? ' admin-form-modal--danger' : '')" :closable="false">
     <div class="admin-form-modal__header">
-        <div class="min-w-0">
+        <div class="admin-form-modal__heading">
+            <span class="admin-form-modal__icon" aria-hidden="true">
+                <flux:icon :icon="$headerIcon" class="size-4" />
+            </span>
+            <div class="min-w-0">
             <h2 class="admin-form-modal__title">{{ $title }}</h2>
             @if (filled($description))
                 <p class="admin-form-modal__description">{{ $description }}</p>
             @endif
+            </div>
         </div>
 
         <flux:modal.close>
